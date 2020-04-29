58664ba85dc82389086abbe0dc97cd172bfb1e33
tmrekk121 <tmrekk121@outlook.jp>
2020-04-30 08:47:26 +0900

    Add Works.html

--- style.js
+++ style.js
@@ -1,9 +1,11 @@
 const about = document.getElementById('about');
+const works = document.getElementById('works');
 
 const about_path = "about.html";
 const works_path = "works.html";
 
 about.addEventListener('click', {file_path: about_path, handleEvent: fetchHTML});
+works.addEventListener('click', {file_path: works_path, handleEvent: fetchHTML});
 
 function fetchHTML(e) {
     var xmlhr = new XMLHttpRequest();
@@ -11,10 +13,10 @@ function fetchHTML(e) {
         if (xmlhr.readyState == 4 && xmlhr.status==200) {
             var content = xmlhr.responseText;
             document.getElementById("slide").setAttribute("style", "display: none;");
+            document.getElementById("item-content").setAttribute("style", "display: block;");
             console.log(this.file_path);
             console.log(content);
             document.getElementById("item-content").innerHTML = content;
-            document.getElementById("item-content").setAttribute("style", "width: 100%; height: 500px;");
         }
     }
     xmlhr.open("GET", this.file_path, true);
