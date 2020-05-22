552173ec96f214b04f59a382aad0c4086dd5c303
tmrekk121 <tmrekk121@outlook.jp>
2020-05-22 10:33:02 +0900

    Rename img file

--- src/javascripts/components/Works.js
+++ src/javascripts/components/Works.js
@@ -5,22 +5,22 @@ export default class Works extends React.Component {
     return (
       <div id="item-content">
         <div className="work-div">
-          <img src="images/portfolio.png" alt="portfolio" className="work-img" />
+          <img src="images/portfolio.jpg" alt="portfolio" className="work-img" />
           <p className="work-title">Portfolio Site</p>
           <p>これまでの経歴や作品を載せたサイト</p>
         </div>
         <div className="work-div">
-          <img src="images/old_portfolio.png" alt="old_portfolio" className="work-img" />
+          <img src="images/old_portfolio.jpg" alt="old_portfolio" className="work-img" />
           <p className="work-title">Old Portfolio Site</p>
           <p>以前使用していたポートフォリオサイト</p>
         </div>
         <div className="work-div">
-          <img src="images/sukuramukun.png" alt="sukuramukun" className="work-img" />
+          <img src="images/sukuramukun.jpg" alt="sukuramukun" className="work-img" />
           <p className="work-title">Sukuramukun</p>
           <p>スクラム開発を支援するアプリ</p>
         </div>
         <div className="work-div">
-          <img src="images/done.png" alt="done" className="work-img" />
+          <img src="images/done.jpg" alt="done" className="work-img" />
           <p className="work-title">DONE</p>
           <p>KPTを円滑にするためのアプリ</p>
         </div>
