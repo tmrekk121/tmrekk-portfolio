abf8e051c5822edba608f23cae8ed4c3739c1e46
tmrekk121 <tmrekk121@outlook.jp>
2020-07-23 09:50:15 +0900

    Update Bottom Message

--- src/javascripts/components/Bottom.js
+++ src/javascripts/components/Bottom.js
@@ -4,7 +4,7 @@ export default class Bottom extends React.Component {
   render() {
     return (
       <div className="bottom-menu">
-        <p className="introduce">Kentaro Sudate. Web Engineer and ...</p>
+        <p className="introduce">Kentaro Sudate. Web Developer and ...</p>
       </div>
     );
   }
